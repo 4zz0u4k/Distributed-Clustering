18968188afcd61f1e9f6c72d84b0dfd4a9ceb554
unknown <la_akeb@esi.dz>
2025-04-18 17:25:06 +0100

    minor addition

--- node.py
+++ node.py
@@ -3,17 +3,19 @@ import socket
 SERVER_IP = '192.168.137.1' 
 PORT = 5000
 
+client_name = input("Your name : ")
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, PORT))
 
 # Optional: initial message to say hello
-client_socket.sendall("ready".encode())
+client_socket.sendall(f"{client_name} is ready".encode())
 
 try:
     while True:
         data = client_socket.recv(1024)
         if not data:
             break
-        print(f"[SERVER] Sent: {data.decode()}")
+        print(f"[SERVER] Sent: {len(data.decode())}")
 finally:
     client_socket.close()
