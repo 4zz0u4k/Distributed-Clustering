e7fc92a369b8c08435c6c1c0a1a356ac463857ac
unknown <la_akeb@esi.dz>
2025-05-03 16:36:00 +0400

    finished the Kmedoides in the head - TODO : PCA and display results + Integrate a CLI + Integrate the metric and run tests

--- server.py
+++ server.py
@@ -19,11 +19,18 @@ data_df = pd.read_csv(DATA_PATH)
 # number of workers
 nodes_info = ray.nodes()
 num_workers = sum(1 for node in ray.nodes() if 'node:__internal_head__' not in node['Resources'])
-print(f"[!] Number of workers : {num_workers}")
 # data spitting
 data_chunks = np.array_split(data_df, num_workers)
 # worker's clustering
 futures = [remote_cluster.remote(chunk) for chunk in data_chunks]
 intermediate_results = ray.get(futures)
-print(f"[+] Intermediate reulsts : (Kmeans) \n {intermediate_results}")
-
+# data unification
+kmeans_centroids = [centroid for centroid in intermediate_results]
+kmeans_centroids_df = pd.DataFrame(np.vstack([centroid for centroid in kmeans_centroids]))
+# head clustering
+kmedoids = KMedoidsCustom(n_clusters=3, random_state=7777)
+kmedoids_labels = kmedoids.fit_predict(kmeans_centroids_df)
+kmedoids_indices = kmedoids.medoid_indices_
+kmedoids_points = kmeans_centroids_df.iloc[kmedoids_indices].values
+print(f"K-medoids points:\n{kmedoids_points}")
+print(f"K-medoids indices: {kmedoids_indices}")
